d4a9c8571498bb2f901e8a5ff38796f30bd03d2b
Elise-Oyi <maryeliseangbo@gmail.com>
2023-09-11 17:19:11 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+
+//-- getting date and time
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+var today = new Date();
+
+const day = weekday[today.getDay()];
+
+// var time = today.getTime()
+
+// console.log(today.getHours())
+
+function getCurrentUTCTimeWithWindow() {
+  const now = new Date();
+  const windowOffset = Math.floor(Math.random() * 240000) - 120000; // +/-2 minutes in milliseconds
+  const currentTimeWithWindow = now.getTime() + windowOffset;
+  const dateWithWindow = new Date(currentTimeWithWindow);
+
+  return dateWithWindow.toISOString();
+}
+
+
+const utcTimeWithWindow = getCurrentUTCTimeWithWindow();
+// console.log(utcTimeWithWindow);
+
+const data = {
+  slack_name: "mary_elise_angbo",
+  track: "backend"
+};
+
+app.get('/get', (req, res) => {
+    const slack_name = req.query.slack_name
+    const track = req.query.track
+
+    if(data.slack_name === slack_name && data.track === track){
+
+        const response = {
+            slack_name: data.slack_name,
+            current_day: day,
+            utc_time: utcTimeWithWindow,
+            track: "backend",
+            github_file_url: 'data.github_file_url',
+            github_repo_url: 'data.github_repo_url',
+            status_code: 200,
+        }
+
+          res.json(response);
+    }
+    else{
+        res.json('Not Found')
+    }
+
+});
+
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}...`);
+  });
+  
